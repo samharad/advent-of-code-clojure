9bbb83ee53a711ca5a557288bc32d6dd56f184b8
Sam Adams <samharad@umich.edu>
2020-12-23 01:55:22 -0500

    22

--- src/aoc/2020/day/22.clj
+++ src/aoc/2020/day/22.clj
@@ -1,5 +1,6 @@
 (ns aoc.2020.day.22
-  (:require [clojure.string :as s]))
+  (:require [clojure.string :as s]
+            [hashp.core]))
 
 (defn ->decks [input]
   (->> (s/split input #"\n\n")
@@ -11,24 +12,60 @@
 
 (def t "Player 1:\n9\n2\n6\n3\n1\n\nPlayer 2:\n5\n8\n4\n7\n10")
 
-(defn round [[p1 p2]]
+(defn add-cards [hand & cs]
+  (vec (concat hand cs)))
+
+(defn combat-round [[p1 p2]]
   (let [[c1 & c1more] p1
-        [c2 & c2more] p2
-        add-cards (fn [hand & cs]
-                    (vec (concat hand cs)))]
+        [c2 & c2more] p2]
     (if (> c1 c2)
       [(add-cards c1more c1 c2) c2more]
       [c1more (add-cards c2more c2 c1)])))
 
-(defn game-result [hands]
+(defn combat [hands]
   (->> hands
-       (iterate round)
+       (iterate combat-round)
        (drop-while #(not (some empty? %)))
        (first)))
 
-(->> (->decks input)
-     (game-result)
-     (some not-empty)
-     (reverse)
-     (map * (rest (range)))
-     (apply +))
+(defn rcombat
+  "Returns {:winner 0 | 1, :hands [p1 p2]}"
+  ([hands] (rcombat hands #{}))
+  ([[h1 h2 :as hands] prev-hands]
+   (let [[c1 & h1more] h1
+         [c2 & h2more] h2
+         prev-hands' (conj prev-hands hands)
+         hands' (fn [winner]
+                  (if (= winner 0)
+                    [(add-cards h1more c1 c2) h2more]
+                    [h1more (add-cards h2more c2 c1)]))]
+     (cond
+       (prev-hands hands) {:winner 0 :hands hands}
+       (empty? h2) {:winner 0 :hands hands}
+       (empty? h1) {:winner 1 :hands hands}
+
+       :else (let [winner (if (and (>= (count h1more) c1)
+                                   (>= (count h2more) c2))
+                            (:winner (rcombat [(take c1 h1more) (take c2 h2more)]))
+                            (if (> c1 c2) 0 1))]
+               (if (= winner 0)
+                 (recur (hands' winner) prev-hands')
+                 (recur (hands' winner) prev-hands')))))))
+
+(defn soln [combat-result]
+  (->> combat-result
+       (some not-empty)
+       (reverse)
+       (map * (rest (range)))
+       (apply +)))
+
+(prn "A" (->> (->decks input)
+              (combat)
+              (soln)))
+
+(time
+  (prn "B"
+    (->> (->decks input)
+         (rcombat)
+         :hands
+         (soln))))
